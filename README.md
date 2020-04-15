a759bad57d3ab269c352c8b68bf854aebcd9b1f0
moisesabraaoso <moisesdirt_bmx@hotmail.com>
2020-04-15 04:39:23 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Flask Search Song
+
+Projetinho simples de busca de Canções usando o Flask.
+
+## Como rodar o Projeto
+
+1. Clone este repositório no terminal
+```bash
+git clone https://github.com/MoisesAbraao/Flask-Search-Song.git
+```
+
+2. No terminal, vá para a pasta raiz do projeto e instale as dependências
+```bash
+pip install -r requirements.txt
+```
+
+3. Ainda no terminal, execute o seguinte comando 
+```bash
+python app.py
+```
+4. Abra o navegador e acesse o sistema 
+```bash
+localhost:5000
+```
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, url_for
+from vagalume import lyrics
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/get_song', methods=['GET', 'POST'])
+def get_song():
+    if request.method == 'POST':
+        banda = request.form.get("banda")
+        cancao = request.form.get("cancao")
+
+        if banda and cancao:
+            resultado = lyrics.find(banda, cancao)
+            song = (resultado.song.lyric)
+    return render_template("index.html", song=song)
+
+if __name__ == "__main__":
+    app.run(debug=True)
